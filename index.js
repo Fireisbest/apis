d5d60e83c5f96cfbcf24081ecf5b2307b3b01e41
Fire <84128345+Fireisbest@users.noreply.github.com>
2024-06-29 16:43:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,32 +29,6 @@ const saveGift = () => {
     fs.writeFileSync(giftsFile, JSON.stringify({ gifts }));
 };
 
-
-const express = require('express');
-const axios = require('axios');
-const fs = require('fs');
-
-const app = express();
-app.use(express.json());
-
-let donations = 1;
-const donationsFile = 'Donations.json';
-let gifts = 1;
-const giftsFile = 'Gifts.json';
-
-if (fs.existsSync(donationsFile)) {
-    const data = fs.readFileSync(donationsFile);
-    donations = JSON.parse(data).donations;
-}
-
-// Function to save the counter to a file
-const saveDonation = () => {
-    fs.writeFileSync(donationsFile, JSON.stringify({ donations }));
-};
-const saveGift = () => {
-    fs.writeFileSync(giftsFile, JSON.stringify({ gifts }));
-};
-
 app.get('/api/gamepasses/:userId/', async (req, res) => {
     const userId = req.params.userId;
     const gamesUrl = `https://games.roblox.com/v2/users/${userId}/games?accessFilter=2&limit=10&sortOrder=Asc`;
@@ -71,7 +45,7 @@ app.get('/api/gamepasses/:userId/', async (req, res) => {
             const games = response.data.data;
             const gamePassesPromises = [];
 
-            games.forEach(game => {
+            for (const game of games) {
                 const gamePassesUrl = `https://games.roblox.com/v1/games/${game.id}/game-passes?limit=10&sortOrder=Asc`;
                 const gamePassesPromise = axios.get(gamePassesUrl, {
                     headers: {
@@ -81,13 +55,13 @@ app.get('/api/gamepasses/:userId/', async (req, res) => {
                 });
 
                 gamePassesPromises.push(gamePassesPromise);
-            });
+            }
 
             const gamePassesResponses = await Promise.all(gamePassesPromises);
             const gamepasses = { GamePasses: [] };
 
             gamePassesResponses.forEach((gamePassesResponse, index) => {
-                if (gamePassesResponse.data && gamePassesResponse.data.data && gamePassesResponse.data.data.length > 0) {
+                if (gamePassesResponse.data && gamePassesResponse.data.data) {
                     const gamePasses = gamePassesResponse.data.data;
                     gamepasses.GamePasses.push({
                         gameId: games[index].id,
@@ -105,22 +79,6 @@ app.get('/api/gamepasses/:userId/', async (req, res) => {
         res.status(500).json({ error: 'An error occurred while fetching game passes' });
     }
 });
-
-app.post('/api/donations', (req, res) => {
-    donations += 1;
-    saveDonation();
-    res.json({ donations });
-});
-
-app.post('/api/gifts', (req, res) => {
-    gifts += 1;
-    saveGift();
-    res.json({ gifts });
-});
-
-module.exports = app;
-module.exports = (req, res) => app(req, res);
-
 
 
 app.post('/api/donations', (req, res) => {
